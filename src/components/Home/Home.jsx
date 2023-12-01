779d17e97a1a4fc479adfd8f01eaecc5e61d3252
saiful islam <saifulislam975317@gmail.com>
2023-12-01 23:22:38 +0600

    added navbar and footer

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "../Shared/Footer/Footer";
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Home;
