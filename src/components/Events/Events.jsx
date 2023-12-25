7176ab7298db2e09c8d83d5ea04fe9ab411e2030
saiful islam <saifulislam975317@gmail.com>
2023-12-25 20:35:22 +0600

    update info

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -2,13 +2,17 @@ import wedding from "../../assets/events/wedding.jpg";
 import dinner from "../../assets/events/dinner.jpg";
 import socialEvents from "../../assets/events/social events.jpg";
 import nonProfit from "../../assets/events/nonprofit.jpg";
-
 const Events = () => {
   return (
     <div className="my-20">
       <h1 className="text-5xl font-bold text-center">
         Plan Your Event With Our <br /> Services
       </h1>
+      <p className="text-center mt-4 mb-12">
+        Gravida nascetur elementum gravida congue netus neque, dui. Sit eget
+        mattis nisi lacus duis <br /> nulla accumsan viverra vulputate. A ut
+        pretium ullamcorper.
+      </p>
       <p className="text-center mt-4 mb-12">
         Gravida nascetur elementum gravida congue netus neque, dui. Sit eget
         mattis nisi lacus duis <br /> nulla accumsan viverra vulputate. A ut
